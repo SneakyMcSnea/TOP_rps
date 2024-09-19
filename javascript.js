7e8613cd07454705b7812f321cbedd4ab09d5c2c
Serafin <138892893+SneakyMcSnea@users.noreply.github.com>
2024-09-19 20:34:14 +0200

    Add getComputerChoice function.
    Add a function to javascript.js that returns either "Rock", "Paper" or "Scissors" as a string.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+function getComputerChoice (){
+    let max = 3;
+    let computerChoice = Math.floor(Math.random() * max);
+    if (computerChoice === 0){
+        return "Rock";
+    }
+    else if (computerChoice === 1){
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
