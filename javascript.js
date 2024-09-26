15f82d5f0e7e016220246b2fdd9433b97baf936f
Serafin <138892893+SneakyMcSnea@users.noreply.github.com>
2024-09-26 15:35:15 +0200

    Add logic to terminate game
    The function checkScores() displays a win/lose message as soon as either
    computerScore or humanScore === 5. The function gets called on every choice
    iteration (= click on button/picture).

--- javascript.js
+++ javascript.js
@@ -46,41 +46,44 @@ function playRoundRock(){
     displayComputerScore.textContent = computerScore;
     }
 
+    checkScores();
 }
 
 function playRoundPaper(){
-let humanChoice = "Paper";
-let computerChoice = getComputerChoice();
+    let humanChoice = "Paper";
+    let computerChoice = getComputerChoice();
 
-const humanSelection = document.querySelector("#user-choice");
-const computerSelection = document.querySelector("#computer-choice");
-const result = document.querySelector("#header");
-const displayHumanScore = document.querySelector("#scoreOfHuman");
-const displayComputerScore = document.querySelector("#scoreOfComputer");
+    const humanSelection = document.querySelector("#user-choice");
+    const computerSelection = document.querySelector("#computer-choice");
+    const result = document.querySelector("#header");
+    const displayHumanScore = document.querySelector("#scoreOfHuman");
+    const displayComputerScore = document.querySelector("#scoreOfComputer");
 
-if (humanChoice === "Paper" && computerChoice === "Rock") {
-    humanSelection.textContent = "YOUR CHOICE: 👋";
-    computerSelection.textContent = "CPU CHOICE: ✊";
-    result.textContent = "YOU WIN!";
-    humanScore++;
-    displayHumanScore.textContent = humanScore;
-    displayComputerScore.textContent = computerScore;
-    }
+    if (humanChoice === "Paper" && computerChoice === "Rock") {
+        humanSelection.textContent = "YOUR CHOICE: 👋";
+        computerSelection.textContent = "CPU CHOICE: ✊";
+        result.textContent = "YOU WIN!";
+        humanScore++;
+        displayHumanScore.textContent = humanScore;
+        displayComputerScore.textContent = computerScore;
+        }
 
-else if (humanChoice === "Paper" && computerChoice === "Paper") {
-    humanSelection.textContent = "YOUR CHOICE: 👋";
-    computerSelection.textContent = "CPU CHOICE: 👋";
-    result.textContent = "TIE!";
-    }   
+    else if (humanChoice === "Paper" && computerChoice === "Paper") {
+        humanSelection.textContent = "YOUR CHOICE: 👋";
+        computerSelection.textContent = "CPU CHOICE: 👋";
+        result.textContent = "TIE!";
+        }   
 
-else if (humanChoice === "Paper" && computerChoice === "Scissors") {
-    humanSelection.textContent = "YOUR CHOICE: 👋";
-    computerSelection.textContent = "CPU CHOICE: ✌";
-    result.textContent = "YOU LOSE!";
-    computerScore++;
-    displayHumanScore.textContent = humanScore;
-    displayComputerScore.textContent = computerScore;
-    }
+    else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        humanSelection.textContent = "YOUR CHOICE: 👋";
+        computerSelection.textContent = "CPU CHOICE: ✌";
+        result.textContent = "YOU LOSE!";
+        computerScore++;
+        displayHumanScore.textContent = humanScore;
+        displayComputerScore.textContent = computerScore;
+        }
+
+    checkScores();
 }
 
 function playRoundScissors(){
@@ -116,55 +119,32 @@ function playRoundScissors(){
         humanSelection.textContent = "YOUR CHOICE: ✌";
         computerSelection.textContent = "CPU CHOICE: ✌";
     }
+
+    checkScores();
 }
 
-
-// function playRound(humanChoice, computerChoice) {
-
-//     const humanSelection = document.querySelector("#user-choice");
-//     const computerSelection = document.querySelector("#computer-choice");
-
-//     if (humanChoice === "Rock" && computerChoice === "Rock") {
-//         humanSelection.textContent = "YOUR CHOICE: ROCK";
-//         computerSelection.textContent = "CPU CHOICE: ROCK";
-//     } else if (humanChoice === "Rock" && computerChoice === "Paper") {
-//         humanSelection.textContent = "YOUR CHOICE: ROCK";
-//         computerSelection.textContent = "CPU CHOICE: PAPER";
-//         computerScore++;
-//     } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-//         humanSelection.textContent = "YOUR CHOICE: ROCK";
-//         computerSelection.textContent = "CPU CHOICE: SCISSORS";
-//         humanScore++
-//     } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-//         console.log("Your choice: " + humanChoice);    
-//         console.log("Computers choice: " + computerChoice);
-//         console.log("You win! Paper beats Rock.");
-//         humanScore++;
-//     } else if (humanChoice === "Paper" && computerChoice === "Paper") {
-//         console.log("Your choice: " + humanChoice);    
-//         console.log("Computers choice: " + computerChoice);
-//         console.log("Tie!");
-//     } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
-//         console.log("Your choice: " + humanChoice);    
-//         console.log("Computers choice: " + computerChoice);
-//         console.log("You lose! Scissors beats Paper.");
-//         computerScore++;
-//     } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
-//         console.log("Your choice: " + humanChoice);    
-//         console.log("Computers choice: " + computerChoice);
-//         console.log("You lose! Rock beats Scissors.");
-//         computerScore++;
-//     } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-//         console.log("Your choice: " + humanChoice);    
-//         console.log("Computers choice: " + computerChoice);
-//         console.log("You win! Scissors beats Paper.");
-//         humanScore++;
-//     } else if (humanChoice === "Scissors" && computerChoice === "Scissors") {
-//         console.log("Your choice: " + humanChoice);    
-//         console.log("Computers choice: " + computerChoice);
-//         console.log("Tie!");
-//     }
-// }
+function checkScores(){
+    if(humanScore === 5){
+        const result = document.querySelector("#header");
+        const displayHumanScore = document.querySelector("#scoreOfHuman");
+        const displayComputerScore = document.querySelector("#scoreOfComputer");
+        result.textContent = "🎉 CONGRATULATIONS, YOU WON THIS GAME! 🎊\n Your score: " + humanScore + "          " + "CPU score: " + computerScore;
+        displayHumanScore.textContent = 0;
+        displayComputerScore.textContent = 0;
+        computerScore = 0;
+        humanScore = 0;
+    }
+    else if (computerScore === 5){
+        const result = document.querySelector("#header");
+        const displayHumanScore = document.querySelector("#scoreOfHuman");
+        const displayComputerScore = document.querySelector("#scoreOfComputer");
+        result.textContent = "🏳😢, YOU LOST THIS GAME 😢🏳\n Your score: " + humanScore + "          " + "CPU score: " + computerScore;
+        displayHumanScore.textContent = 0;
+        displayComputerScore.textContent = 0;
+        computerScore = 0;
+        humanScore = 0;
+    }
+}
 
 let computerScore = 0;
 let humanScore = 0;
